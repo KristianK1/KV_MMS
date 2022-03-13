cb66c1499a8a4ed0f4389906c7f3d86222824565
KristianK1 <kikihd11@gmail.com>
2022-03-14 00:39:18 +0100

    readFilePaths gotov

--- readFilePaths.m
+++ readFilePaths.m
@@ -1,31 +1,60 @@
 function files = readFilePaths(initialPath, foldersToExclude, extention)
-    if(initialPath == 1)
-        cd "voices_repo\testData";
-    end
-    %files = string([50000,1]);
-    resultSize = 0;
-    dirs = dir;
+
+%funkcija vraca popis svih failova unutar "initialPath" foldera koji
+%zavrsavaju na "extention" osim ako se nalazi unutar foldera koji je
+%naveden u "foldersToExclude"
+
+%povratni argument je "string array"
+%kad se zavrsi funckija rezultat se "cita" na sljedeci nacin:
+
+%result = readFilePaths(initialPath, folders, "mp3")
+
+%sizeArray = size(result);
+%sizeArray = sizeArray(1,1);
+%for i=1:sizeArray
+%    result(i)
+%end
+
+
+%prvi argument je tipa
+% q = "D:\KV_MMS\voices_repo\testData_easyNames";
+
+%drugi argument je tipa
+%q = ["female", "teens", "ime_foldera"];
+%pravilo je da ignorira SVE foldere s tim imenom, cak i ako je folder
+%nalazi unutar "dopustenog" folfera
+
+%treci argument je tipa
+%q= "mp3" (ili ".mp3")
+%pravilo je da "ubere" file ako zavrsava na taj string (endsWith funkcija)
+    dirs = dir(initialPath);
     tempSize = size(dirs);
     N = tempSize(1,1);
+    files = strings([0,1]);
     for i=1:N
         if(dirs(i).isdir == true)
-            tempSize = size(foldersToExclude);
-            NfoldersEx = tempSize(1,1);
+            tempSize = size(foldersToExclude)
+            NfoldersEx = tempSize(1,2);
             excludeThis = 0;
+            dirs(i).name
+            if strcmp(dirs(i).name, ".") || strcmp(dirs(i).name, "..")
+                excludeThis = 1;
+                "remove " + dirs(i).name
+            end
             for j=1:NfoldersEx
                 if strcmp(dirs(i).name, foldersToExclude(j))
                     excludeThis = 1;
+                    "remove " + dirs(i).name
                 end
+                
             end
             if(excludeThis == 0)
-                cd(dirs(i));
-                    filesFromFolder = readFilePaths(0, foldersToExclude, extention);
-                    files = [files; filesFromFolder]
-                cd ..
+                path = initialPath + "\" + dirs(i).name
+                filesFromFolder = readFilePaths(path, foldersToExclude, extention);
+                files = [files; filesFromFolder];
             end
         elseif endsWith(dirs(i).name, extention) == true
-            resultSize = resultSize + 1;
-            files(resultSize) = dirs(i).name;
+            files = [files; dirs(i).name];
         else
             "some useless file"
             dirs(i).name
--- testing_readFilePaths.m
+++ testing_readFilePaths.m
@@ -1,4 +1,10 @@
 clc
+folders = ["f1", "f22", "f31"];
+initialPath ="D:\KV_MMS\voices_repo\testData_easyNames";
+result = readFilePaths(initialPath, folders, "mp3")
 
-folders = ["f1", "f22"];
-result = readFilePaths(1, folders, "mp3")
+sizeArray = size(result);
+sizeArray = sizeArray(1,1);
+for i=1:sizeArray
+    result(i)
+end
