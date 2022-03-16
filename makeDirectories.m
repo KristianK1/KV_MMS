7088afc81badf7ad0f58803d15299771f8e2d3fa
KristianK1 <kikihd11@gmail.com>
2022-03-16 01:21:58 +0100

    updating file filtering
    before adding matlab toolbox "Statistics and machine learning"

---
+++ makeDirectories.m
@@ -0,0 +1,13 @@
+function makeDirectories(destination, folders)
+    %destination -> "D:\KV_MMS" - for example
+    %folders -> horizontal array of folderPaths that need to be made ->
+    %["dir1", "dir2", "dir1\dir2\dir3"] 
+    sizee = size(folders);
+    sizee = sizee(1,1);
+    for i=1:sizee
+        directoryPath = destination + "\" + folders(i,1)
+        if ~exist(directoryPath, 'dir')
+            mkdir(directoryPath);
+        end
+    end
+end
