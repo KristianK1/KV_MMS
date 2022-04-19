4c336117da1a1eae6972480a16def6cd611b3700
Damir173 <basicdamir12@gmail.com>
2022-04-19 13:39:42 +0200

    freq ages

--- freq_ages.m
+++ freq_ages.m
@@ -3,18 +3,35 @@ clc
 
 ageGroups = ["teens", "twenties", "thirties", "fourties", "fifties", "sixties", "seventies", "eighties", "nineties"];
 
-path ="D:\KV_MMS_voices_repo\mozilla8_repeat\male"; %PC Kristian
+path ="D:\KV_MMS_voices_repo\mozilla8_repeat"; %PC Kristian
+%path ="D:\KV_MMS_voices_repo\mozilla"; %laptop Damir
 
-N = ;
+
+
+N = 100;
 figure
-sumTotal = {};
 for i=1:9
     foldersIgnore = ageGroups;
     foldersIgnore(i) = "";
     foldersIgnore
-    [rez,f] = loadReadAnalize(path, foldersIgnore, ".mp3", N, 0.5,61,3000);
+    [rez,f] = loadReadAnalize(path + "\male", foldersIgnore, ".mp3", N, 0.5,61,3000);
     rezSum = transpose(sum((transpose(rez))));
     subplot(3,3,i);
     plot(f, rezSum);
-    title(ageGroups(i));
+    title(ageGroups(i) + " male");
+end
+
+
+
+N = 100;
+figure
+for i=1:9
+    foldersIgnore = ageGroups;
+    foldersIgnore(i) = "";
+    foldersIgnore
+    [rez,f] = loadReadAnalize(path + "\female", foldersIgnore, ".mp3", N, 0.5,61,3000);
+    rezSum = transpose(sum((transpose(rez))));
+    subplot(3,3,i);
+    plot(f, rezSum);
+    title(ageGroups(i) + " female");
 end
